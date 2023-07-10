4e0a5842d45b49192f12c29ca745667f0700ecda
akrck02 <aketza.vazquez@gmail.com>
2023-07-10 18:29:23 +0200

    [docker] this is an adventure

--- docker/production/start.sh
+++ docker/production/start.sh
@@ -1,5 +1,26 @@
+# Download code from Github
+git clone https://github.com/akrck02/ServerDashboard.git && echo "Downloaded code from Github"
+
+# install dependencies
+cd ServerDashboard && npm i && echo "Installed dependencies"
+
+# Compile source files
+npm run compile-client && echo "Compiled source files"
+
+# pack styles and scripts
+npm run pack && echo "Packed styles and scripts"
+
+# build go app
+cd backend && go build -o server-dashboard-api && echo "Built go app"
+
+# change to production mode
+cd ../ && npm run prod && echo "Changed to production mode"
+
+# remove source files
+rm -rf app/indexDev.html app/styles/src frontend/src frontend/temp frontend/tsconfig.json styles.config env.js Readme.md bin .github && echo "Removed source files"
+
 # add execution permissions
-chmod +x /app/ServerDashboard/backend/server-dashboard-api
+chmod +x backend/server-dashboard-api && echo "Added execution permissions"
 
 # run go app
-cd /app/ServerDashboard/backend && ./server-dashboard-api & npm run serve
+ls -la && npm run serve & ./backend/server-dashboard-api && echo "App is running!"
